bf7d8367b9da6724c52b2dca8efe73a38bf8df93
SergDC101 <sergdc101@gmail.com>
2023-08-08 22:42:17 +0300

    first commit

---
+++ config/Database.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class Database{
+
+    // Database Properties.
+    private $host = 'localhost';
+    private $db_name = 'api';
+    private $username = 'root';
+    private $password = '';
+    private $connection = null;
+
+
+    // Function for making connection to database.
+    
+    public function connect()
+    {
+        try
+        {
+            $this-> connection = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name,
+                                        $this-> username,
+                                        $this-> password,
+        );
+        }
+        catch(PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+        return $this->connection;
+
+
+    }
+
+
+
+}
---
+++ models/Post.php
@@ -0,0 +1,55 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_error', 1);
+
+
+class Post{
+
+    // Post Properties.
+    public $id;
+    public $category_id;
+    public $title;
+    public $description;
+    public $create_at;
+
+    // Database Data.
+
+    private $connection;
+    private $table = 'posts';
+
+    public function __construct($db)
+    {
+        $this->connection = $db;
+    }
+
+    
+    // Method to read all the saved posts from database.
+
+    public function readPosts()
+    {
+        // Query for reading posts from table.
+
+        $query = 'SELECT 
+            category.name as category,
+            posts.id,
+            posts.title,
+            posts.category_id,
+            posts.description,
+            posts.created_at
+            FROM '.$this->table.' posts LEFT JOIN
+            category ON posts.category_id = category.id
+            ORDER BY
+            posts.created_at DESC
+        ';
+
+        $post = $this->connection->prepare($query);
+        
+        $post->execute();
+        
+        return $post;
+    }
+
+}
+
+
